01791d3d4f007d8b730488c1d757ce9a36010cd6
Carlos Costa <carllos.nc@gmail.com>
2023-01-03 21:49:18 -0300

    add: local repository implementation

--- lib/data/repositories/local.repository.dart
+++ lib/data/repositories/local.repository.dart
@@ -1,34 +1,57 @@
+import '/data/database/local_database.dart';
 import '/domain/entities/notification.entity.dart';
 import '/domain/repository/notification.repository.dart';
 
 class LocalRepository implements NotificationRepository {
+  final LocalDatabase database;
+
+  LocalRepository(this.database);
+
   @override
   Future<int> create(Notification entry) {
-    // TODO: implement create
-    throw UnimplementedError();
+    var data = NotificationLocalCompanion.insert(
+      name: entry.name,
+      schedule: entry.schedule,
+    );
+
+    return database.into(database.notificationLocal).insert(data);
   }
 
   @override
   Future<int> delete(Notification entry) {
-    // TODO: implement delete
-    throw UnimplementedError();
+    var data = NotificationLocalCompanion.insert(
+      name: entry.name,
+      schedule: entry.schedule,
+    );
+
+    return database.into(database.notificationLocal).insert(data);
   }
 
   @override
   Stream<Notification> get({required String id}) {
-    // TODO: implement get
-    throw UnimplementedError();
+    var item = database.select(database.notificationLocal)
+      ..where(
+        (table) => table.id.equals(
+          int.parse(id),
+        ),
+      );
+
+    return item.watchSingle() as Stream<Notification>;
   }
 
   @override
   Stream<List<Notification>> read() {
-    // TODO: implement read
-    throw UnimplementedError();
+    return database.select(database.notificationLocal).watch()
+        as Stream<List<Notification>>;
   }
 
   @override
   Future<bool> update(Notification entry) {
-    // TODO: implement update
-    throw UnimplementedError();
+    var data = NotificationLocalCompanion.insert(
+      name: entry.name,
+      schedule: entry.schedule,
+    );
+
+    return database.update(database.notificationLocal).replace(data);
   }
 }
