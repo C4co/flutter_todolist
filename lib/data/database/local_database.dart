ea9ecfb209ab992a31a8dfaf2e0a1de84b55e7dd
Carlos Costa <carllos.nc@gmail.com>
2023-01-05 10:52:19 -0300

    rename: migrations to tables

--- lib/data/database/local_database.dart
+++ lib/data/database/local_database.dart
@@ -4,7 +4,7 @@ import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
-import '/data/migrations/notification_local_table.dart';
+import '/data/tables/notification_local_table.dart';
 
 part 'local_database.g.dart';
 
