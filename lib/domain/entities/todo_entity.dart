75a6e78ab793e5152a4ed62ebf0e666a2b798c2d
Carlos Costa <carllos.nc@gmail.com>
2023-01-09 19:59:51 -0300

    update: domain layer

--- lib/domain/entities/todo_entity.dart
+++ lib/domain/entities/todo_entity.dart
@@ -1,13 +1,9 @@
 class Todo {
   String name;
-  int hour;
-  int minute;
   String description;
 
   Todo({
     required this.name,
-    required this.hour,
-    required this.minute,
     required this.description,
   });
 }
